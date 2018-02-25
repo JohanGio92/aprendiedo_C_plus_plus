8f1f5ba5bec93c66ba79c086fe7ab123d76e89f2
Johan Revilla <johan_revilla_92@hotmail.com>
2018-02-25 17:58:39 -0300

    "cambie salto de linea \n por endl"

---
+++ entrada_salida_de_datos.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int unNumero;
+  cout << "digite un numero por favor\n";
+  cin >> unNumero;
+  cout << "numero = " << unNumero << endl;
+  return 0;
+}
